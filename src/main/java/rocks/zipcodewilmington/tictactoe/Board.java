c7ec1d6b95122dcd835a58ec8601059cabab9de7
Szeng89 <shijinz898@gmail.com>
2024-03-12 16:20:50 -0400

    added for some diagoanls

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -11,42 +11,77 @@ public class Board {
 
     public Boolean isInFavorOfX() {
         int xCount = 0;
-        boolean rows = false;
+        boolean xWins = false;
 
-        for (int i = 0; i < matrix.length; i ++){
-            for(int j = 0; j < matrix.length; j++){
-                if(matrix[i][j] == 'X') {
+
+        for (int i = 0; i < matrix.length; i ++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[i][j] == 'X') {
                     xCount++;
                 }
 
             }
-            if (xCount == 3){
-                rows = true;
+            if (xCount == 3) {
+                xWins = true;
+                xCount = 0;
+            }
+
+
+            // Diagonals
+            if (matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X') {
+                xWins = true;
+
+            }
+
+            if (matrix[2][0] == 'X' && matrix[1][1] == 'X' && matrix[0][2] == 'X') {
+                xWins = true;
+
             }
         }
-        return rows;
+
+        return xWins;
     }
 
     public Boolean isInFavorOfO() {
         int oCount = 0;
-        boolean rows = false;
+        boolean Owins = false;
 
-        for (int i = 0; i < matrix.length; i ++){
-            for(int j = 0; j < matrix.length; j++){
-                if(matrix[i][j] == 'O') {
+
+        for (int i = 0; i < matrix.length; i ++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[i][j] == 'O') {
                     oCount++;
                 }
 
             }
-            if (oCount == 3){
-                rows = true;
+            if (oCount == 3) {
+                Owins = true;
+                oCount = 0;
+            }
+
+
+            // Diagonals
+            if (matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O') {
+                Owins = true;
+
+            }
+
+            if (matrix[2][0] == 'O' && matrix[1][1] == 'O' && matrix[0][2] == 'O') {
+                Owins = true;
+
             }
         }
-        return rows;
+
+        return Owins;
     }
 
     public Boolean isTie() {
-        return null;
+        boolean tie = false;
+        if(isInFavorOfX() == isInFavorOfO()){
+            tie = true;
+        }
+        return tie;
+
     }
 
     public String getWinner() {
